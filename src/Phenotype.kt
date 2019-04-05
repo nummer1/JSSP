c735e99061aecfd9f7e700832ca1f17f1950824b
nummer1 <AnonymNN2@gmail.com>
2019-04-05 20:24:48 +0200

    finished gantt-chart, added bees algorithm and Particle swarm templates

--- src/Phenotype.kt
+++ src/Phenotype.kt
@@ -1,8 +1,8 @@
+import java.awt.BasicStroke
 import java.awt.image.BufferedImage
 import kotlin.math.max
 import java.io.File
 import javax.imageio.ImageIO
-import javax.swing.Spring.height
 import java.awt.Rectangle
 import java.awt.Color
 
@@ -47,48 +47,69 @@ class Phenotype(val problem: Problem) {
         }
     }
 
+    fun getFitness(): Int {
+        var time = 0
+        for ((i, list) in schedule.withIndex()) {
+            if (problem.jobs[i].last().second + list.last() > time) {
+                time = problem.jobs[i].last().second + list.last()
+            }
+        }
+        return time
+    }
+
     fun plot() {
-        // TODO: plot in Gantt-Chart
-        // TODO: widht should correspond to fitness
+        val colorList = mutableListOf("#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#800000",
+            "#008000", "#000080", "#808000", "#800080", "#008080", "#808080", "#C00000", "#00C000", "#0000C0", "#C0C000",
+            "#C000C0", "#00C0C0", "#C0C0C0", "#400000", "#004000", "#000040", "#404000", "#400040", "#004040", "#404040",
+            "#200000", "#002000", "#000020", "#202000", "#200020", "#002020", "#202020", "#600000", "#006000", "#000060",
+            "#606000", "#600060", "#006060", "#606060", "#A00000", "#00A000", "#0000A0", "#A0A000", "#A000A0", "#00A0A0",
+            "#A0A0A0", "#E00000", "#00E000", "#0000E0", "#E0E000", "#E000E0", "#00E0E0", "#E0E0E0")
 
-        val colorList = mutableListOf("FF0000", "00FF00", "0000FF", "FFFF00", "FF00FF", "800000",
-            "008000", "000080", "808000", "800080", "008080", "808080", "C00000", "00C000", "0000C0", "C0C000",
-            "C000C0", "00C0C0", "C0C0C0", "400000", "004000", "000040", "404000", "400040", "004040", "404040",
-            "200000", "002000", "000020", "202000", "200020", "002020", "202020", "600000", "006000", "000060",
-            "606000", "600060", "006060", "606060", "A00000", "00A000", "0000A0", "A0A000", "A000A0", "00A0A0",
-            "A0A0A0", "E00000", "00E000", "0000E0", "E0E000", "E000E0", "00E0E0", "E0E0E0")
-
-        val width = 200
-        val height = problem.numberMachines * 24 + 6
+        val width = 26 + 10 * (getFitness() + 2)
+        val height = problem.numberMachines * 24 + 6 + 16
         val img = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
 
         // draw white background
         val graph = img.createGraphics()
-        graph.setColor(Color.WHITE)
+        graph.color = Color.WHITE
         graph.fill(Rectangle(0, 0, width, height))
 
         // draw machine boxes
-        graph.setColor(Color.LIGHT_GRAY)
         for (i in 0.until(problem.numberMachines)) {
+            graph.color = Color.LIGHT_GRAY
             graph.fill(Rectangle(6, i * 24 + 6, 18, 18))
+            graph.color = Color.BLACK
+            graph.drawString("M$i", 6, (i+1) * 24 - 6)
         }
 
         // draw horisontal lines
-        graph.setColor(Color.BLACK)
+        graph.color = Color.DARK_GRAY
         for (i in 0.until(problem.numberMachines + 1)) {
             graph.drawLine(26, i*24 + 3, width, i*24 + 3)
         }
 
         // draw vertical lines
-        graph.setColor(Color.BLACK)
-        for (i in 0.until(10)) {
-
+        for (i in 0.until((width - 26)/10)) {
+            if (i%5 == 0) {
+                graph.color = Color.BLACK
+                graph.drawString("$i", i*10 + 20, height-3)
+                graph.stroke = BasicStroke(2.toFloat())
+            }
+            graph.color = Color.DARK_GRAY
+            graph.drawLine(i*10 + 26, 3, i*10 + 26, height-3-16)
+            graph.stroke = BasicStroke(1.toFloat())
         }
 
         // draw job boxes
-        for (job in schedule) {
-            graph.setColor(Color.GREEN)
-
+        for ((i, job) in schedule.withIndex()) {
+            for ((k, j) in job.withIndex()) {
+                val machine = problem.jobs[i][k].first
+                val time = problem.jobs[i][k].second
+                graph.color = Color.decode(colorList[i])
+                graph.fill(Rectangle(j*10 + 26, machine*24 + 6, time*10, 18))
+                graph.color = Color.BLACK
+                graph.drawString("($k/$i)", j*10 + 26, (1+machine)*24 - 6)
+            }
         }
         graph.dispose()
 
--- src/main.kt
+++ src/main.kt
@@ -1,9 +1,12 @@
 // TODO: deadlock?
+// NOTE: more than 50 jobs crashes because of colourlist in Phenotype.plot()
 
 
 fun main(args: Array<String>) {
     val p = Problem()
     p.read("Test Data/1.txt")
     val pheno = Phenotype(p)
+    pheno.fromSequence(mutableListOf(0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5))
+    println(pheno.getFitness())
     pheno.plot()
 }
